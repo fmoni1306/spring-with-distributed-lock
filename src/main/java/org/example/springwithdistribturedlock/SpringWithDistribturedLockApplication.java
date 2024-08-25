ddea010cb211b5770ad97e314c0770ea13f8bcac
Yoon <fmoni1306@gmail.com>
2024-08-25 23:20:17 +0900

    init

---
+++ src/main/java/org/example/springwithdistribturedlock/SpringWithDistribturedLockApplication.java
@@ -0,0 +1,13 @@
+package org.example.springwithdistribturedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithDistribturedLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWithDistribturedLockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springwithdistribturedlock/SpringWithDistribturedLockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springwithdistribturedlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithDistribturedLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
